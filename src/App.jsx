5b61fac6241991d510d7d74df8e1022cb71b4efc
shamilshiraz <shamilshiraz0@gmail.com>
2025-03-07 15:52:51 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import React, { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
-import Nav from './components/Nav'
-import './App.css'
-import Hero from './components/Hero'
-import Page from './components/Sc'
-import Intro from './components/Intro'
-import SVGUnmaskingEffect from './components/Svg'
-import VideoGrid from './components/Video'
-import Stack from './components/Stack'
-import Example from './components/Horizontal'
-import Mos from './components/Mos'
-import Footer from './components/Footer'
-import LargeClientScroller from './components/Clients'
-import BulgeTextEffect from './components/Bg'
-import Exmmple from './components/Hero'
-import TextSkewAnimation from './components/Try'
-import TextEx from './components/Try'
-import Scene from './components/Scene'
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import Lenis from '@studio-freight/lenis';
+import Nav from './components/Nav';
+import Hero from './components/Hero';
+import Page from './components/Sc';
+import Intro from './components/Intro';
+import SVGUnmaskingEffect from './components/Svg';
+import VideoGrid from './components/Video';
+import Stack from './components/Stack';
+import Example from './components/Horizontal';
+import Mos from './components/Mos';
+import Footer from './components/Footer';
+import LargeClientScroller from './components/Clients';
+import BulgeTextEffect from './components/Bg';
+import Exmmple from './components/Hero';
+import TextSkewAnimation from './components/Try';
+import TextEx from './components/Try';
+import Scene from './components/Scene';
 
 function App() {
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMouseX(e.clientX);
+      setMouseY(e.clientY);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   // Set up Lenis on component mount
   useEffect(() => {
     const lenis = new Lenis({
@@ -29,44 +45,48 @@ function App() {
       smooth: true,
       smoothTouch: false,
       touchMultiplier: 2,
-    })
+    });
 
-    
-
-    // Bind lenis to requestAnimationFrame
     function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
-    
-    requestAnimationFrame(raf)
 
-    // Make lenis available globally for other components
-    window.lenis = lenis
+    requestAnimationFrame(raf);
+    window.lenis = lenis;
 
     return () => {
-      lenis.destroy()
-      window.lenis = null
-    }
-  }, [])
+      lenis.destroy();
+      window.lenis = null;
+    };
+  }, []);
 
   return (
-    <div>
-      <Nav/>
-      <Scene/>
-      {/* <TextEx/> */}
-      <Exmmple/>
-      {/* <BulgeTextEffect/> */}
-      {/* <Page/> */}
-      <VideoGrid/>
-      {/* <SVGUnmaskingEffect/> */}
-      <Intro/>
-      <Stack/>
-      <Example/>
-      <LargeClientScroller/>
-      <Mos/>
+    <div className="relative">
+      {/* Custom Cursor */}
+      <motion.div
+        className="fixed top-0 left-0 w-3 h-3 bg-white rounded-full pointer-events-none z-[9999]"
+        animate={{ x: mouseX - 6, y: mouseY - 6 }}
+        transition={{ type: 'spring', stiffness: 500, damping: 28 }}
+      />
+      <motion.div
+        className="fixed top-0 left-0 w-12 h-12 border border-white rounded-full pointer-events-none z-[9998]"
+        animate={{ x: mouseX - 24, y: mouseY - 24 }}
+        transition={{ type: 'spring', stiffness: 100, damping: 15 }}
+      />
+
+      {/* Page Content */}
+      <Nav />
+      <Scene />
+      <Exmmple />
+      <VideoGrid />
+      <Intro />
+      <Stack />
+      <Example />
+      <LargeClientScroller />
+      <Mos />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
